aae2fa6d9a8478ca4eac9748b35c5099c50e5607
mikusi08 <mikusi08@gmail.com>
2012-09-07 16:11:50 +0900

    #9/7 Fri retry nisikawa

--- src/oit/iloop/kiosk/kiosk_bus/package-info.java
+++ src/oit/iloop/kiosk/kiosk_bus/package-info.java
@@ -5,4 +5,11 @@
  * @author katoumasanori
  *
  */
+
+/**
+ * day:9/7 Fri
+ * @author nisikawa tomoya
+ * make BusMain.java , BusController.java
+ * **/
+
 package oit.iloop.kiosk.kiosk_bus;
--- src/oit/iloop/kiosk/kiosk_examination/package-info.java
+++ src/oit/iloop/kiosk/kiosk_examination/package-info.java
@@ -5,10 +5,5 @@
  * @author katoumasanori
  *
  */
-/**
- * day:9/7 Fri
- * @author nisikawa tomoya
- * make BusMain.java , BusController.java
- * **/
 
 package oit.iloop.kiosk.kiosk_examination;
