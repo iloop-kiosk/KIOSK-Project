482dbd178f03e571a3f0dbab244a768f05274b34
mikusi08 <mikusi08@gmail.com>
2012-09-11 14:01:19 +0900

    #9/10(Tue) BusMain.java update @actor nisikawa

--- src/oit/iloop/kiosk/kiosk_bus/BusMain.java
+++ src/oit/iloop/kiosk/kiosk_bus/BusMain.java
@@ -2,3 +2,27 @@
  *バスページのメインクラス 
  */
 package oit.iloop.kiosk.kiosk_bus;
+
+
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+public class BusMain extends Parent {
+	
+	public BusMain() throws IOException{
+		
+		/*FXMLファイルの読み込み*/
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("businfo.fxml"));
+		
+		/**/
+		Parent fxmlSchene = (Parent)fxmlLoader.load();
+		
+		/**/
+		getChildren().add(fxmlSchene);
+		
+//		InfoController infoController = (InfoController)fxmlLoader.getController("");
+		
+	}
+}
